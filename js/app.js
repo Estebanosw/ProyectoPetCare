24ed92e7355d3aeae3b106b6ee5ae1a4bccfcd1e
Esteban Oswaldo Prieto Riaño <oswaldo@appinit.co>
2024-10-30 20:10:20 -0500

    cambios en estilos y adapdación de node para las API

--- js/app.js
+++ js/app.js
@@ -1,34 +1,12 @@
-document.getElementById("registroForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
+const express = require('express');
+const connection = require('./db');
+const app = express();
 
-    const cedula = document.getElementById("cedula").value;
-    const nombre = document.getElementById("nombre").value;
-    const edad = document.getElementById("edad").value;
-    const profesion = document.getElementById("profesion").value;
+// Encargado de parsear a los json
+app.use(express.json());
 
-    try {
-        const response = await fetch("http://localhost:3000/api/guardar", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                Cedula: cedula,
-                Nombre: nombre,
-                Edad: edad,
-                Profesion: profesion,
-            }),
-        });
-
-        const result = await response.json();
-
-        if (response.ok) {
-            document.getElementById("responseMessage").textContent = "Registro guardado exitosamente";
-            document.getElementById("registroForm").reset();
-        } else {
-            document.getElementById("responseMessage").textContent = "Error al guardar el registro: " + result.error;
-        }
-    } catch (error) {
-        document.getElementById("responseMessage").textContent = "Error de conexión: " + error.message;
-    }
+// Puerto de Conexión del servidor
+const PORT = 3000;
+app.listen(PORT, () =>{
+    console.log('Servidor Corriendo');
 });
